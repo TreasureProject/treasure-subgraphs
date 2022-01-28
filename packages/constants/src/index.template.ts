e34f13b122aaa1089f5f3f34cff24d40088e2ac6
Neil Kistner <neil.kistner@gmail.com>
2022-01-28 06:46:18 -0600

    Update ABIs and configuration packages

--- packages/constants/src/index.template.ts
+++ packages/constants/src/index.template.ts
@@ -1,6 +1,8 @@
 import { Address } from "@graphprotocol/graph-ts";
 
+export const BURNER_ADDRESS = Address.fromString("{{ burner_address }}");
 export const CONSUMABLE_ADDRESS = Address.fromString("{{ consumable_address }}");
 export const CRAFTING_ADDRESS = Address.fromString("{{ crafting_address }}");
+export const EXPLORER = "{{ explorer }}";
 export const LEGION_ADDRESS = Address.fromString("{{ legion_address }}");
-export const TREASURE_ADDRESS = Address.fromString('{{ treasure_address }}')
+export const TREASURE_ADDRESS = Address.fromString("{{ treasure_address }}");
