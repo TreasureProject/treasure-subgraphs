35d8c7bb59f31bcfc565efbb98334e9b21bd13e6
Neil Kistner <neil.kistner@gmail.com>
2022-02-16 09:35:48 -0600

    [bridgeworld] handle multiple withdrawals against one deposit (#57)

---
+++ subgraphs/bridgeworld/tests/helpers/atlas-mine.ts
@@ -0,0 +1,50 @@
+import { Address, BigInt, ethereum } from "@graphprotocol/graph-ts";
+import { newMockEvent } from "matchstick-as";
+import { ATLAS_MINE_ADDRESS, Lock, toBigIntString } from ".";
+import { Deposit, Withdraw } from "../../generated/Atlas Mine/AtlasMine";
+
+export const createDepositEvent = (
+  user: string,
+  depositId: i32,
+  amount: number,
+  lock: i32 = Lock.TwoWeeks
+): Deposit => {
+  const newEvent = changetype<Deposit>(newMockEvent());
+  newEvent.address = Address.fromString(ATLAS_MINE_ADDRESS);
+  newEvent.parameters = [
+    new ethereum.EventParam(
+      "user",
+      ethereum.Value.fromAddress(Address.fromString(user))
+    ),
+    new ethereum.EventParam("index", ethereum.Value.fromI32(depositId)),
+    new ethereum.EventParam(
+      "amount",
+      ethereum.Value.fromSignedBigInt(BigInt.fromString(toBigIntString(amount)))
+    ),
+    new ethereum.EventParam("lock", ethereum.Value.fromI32(lock)),
+  ];
+
+  return newEvent;
+};
+
+export const createWithdrawEvent = (
+  user: string,
+  depositId: i32,
+  amount: number
+): Withdraw => {
+  const newEvent = changetype<Withdraw>(newMockEvent());
+  newEvent.address = Address.fromString(ATLAS_MINE_ADDRESS);
+  newEvent.parameters = [
+    new ethereum.EventParam(
+      "user",
+      ethereum.Value.fromAddress(Address.fromString(user))
+    ),
+    new ethereum.EventParam("index", ethereum.Value.fromI32(depositId)),
+    new ethereum.EventParam(
+      "amount",
+      ethereum.Value.fromSignedBigInt(BigInt.fromString(toBigIntString(amount)))
+    ),
+  ];
+
+  return newEvent;
+};
--- subgraphs/bridgeworld/tests/helpers/index.ts
+++ subgraphs/bridgeworld/tests/helpers/index.ts
@@ -1,6 +1,12 @@
+export * from "./atlas-mine";
 export * from "./constants";
 export * from "./crafting";
 export * from "./legion";
+export * from "./magic";
 export * from "./pilgrimage";
 export * from "./questing";
 export * from "./randomizer";
+
+export function toBigIntString(value: number): string {
+  return `${value * 1e18}`.replace(".0", "");
+}
