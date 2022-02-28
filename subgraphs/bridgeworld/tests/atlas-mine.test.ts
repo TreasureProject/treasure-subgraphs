456fc7c15e892777a12f25b7ea65dd3ce5c4b268
Neil Kistner <neil.kistner@gmail.com>
2022-02-28 11:44:44 -0600

    [bridgeworld] support multisig with (un)staking in Atlas Mine (#92)

--- subgraphs/bridgeworld/tests/atlas-mine.test.ts
+++ subgraphs/bridgeworld/tests/atlas-mine.test.ts
@@ -1,18 +1,31 @@
-import { assert, clearStore, test } from "matchstick-as/assembly";
+import { assert, clearStore, logStore, test } from "matchstick-as/assembly";
 
 import { Address } from "@graphprotocol/graph-ts";
 
-import { handleDeposit, handleWithdraw } from "../src/mappings/atlas-mine";
+import { TREASURE_ADDRESS } from "@treasure/constants";
+
+import {
+  handleDeposit,
+  handleStaked,
+  handleUnstaked,
+  handleWithdraw,
+} from "../src/mappings/atlas-mine";
 import { handleApproval } from "../src/mappings/magic";
+import { handleTransferBatch } from "../src/mappings/treasure";
 import {
   ATLAS_MINE_ADDRESS,
   DEPOSIT_ENTITY_TYPE,
   Lock,
+  MULTISIG_ADDRESS,
+  STAKED_TOKEN_ENTITY_TYPE,
   USER_ADDRESS,
   USER_ENTITY_TYPE,
   WITHDRAW_ENTITY_TYPE,
   createApprovalEvent,
   createDepositEvent,
+  createStakedEvent,
+  createTreasureTransferEvent,
+  createUnstakedEvent,
   createWithdrawEvent,
   toBigIntString,
 } from "./helpers/index";
@@ -67,4 +80,161 @@ test("withdrawals will add to existing when depositId matches", () => {
   assert.fieldEquals(WITHDRAW_ENTITY_TYPE, id, "amount", toBigIntString(3));
   assert.fieldEquals(WITHDRAW_ENTITY_TYPE, id, "deposit", id);
   assert.fieldEquals(WITHDRAW_ENTITY_TYPE, id, "user", USER_ADDRESS);
+});
+
+test("stake/unstake handles when a user does the event", () => {
+  clearStore();
+
+  const mintEvent = createTreasureTransferEvent(
+    Address.zero().toHexString(),
+    USER_ADDRESS,
+    [95],
+    [1]
+  );
+
+  handleTransferBatch(mintEvent);
+
+  let transferEvent = createTreasureTransferEvent(
+    USER_ADDRESS,
+    ATLAS_MINE_ADDRESS,
+    [95],
+    [1]
+  );
+
+  handleTransferBatch(transferEvent);
+
+  const stakedEvent = createStakedEvent(
+    USER_ADDRESS,
+    TREASURE_ADDRESS,
+    95,
+    1,
+    0.157
+  );
+
+  handleStaked(stakedEvent);
+
+  assert.fieldEquals(USER_ENTITY_TYPE, USER_ADDRESS, "boost", "0.157");
+  assert.fieldEquals(USER_ENTITY_TYPE, USER_ADDRESS, "boosts", "1");
+
+  const tokenId = `${TREASURE_ADDRESS.toHexString()}-0x${(95).toString(16)}`;
+  const stakedId = `${USER_ADDRESS}-${tokenId}`;
+
+  assert.fieldEquals(USER_ENTITY_TYPE, USER_ADDRESS, "staked", `[${stakedId}]`);
+  assert.fieldEquals(STAKED_TOKEN_ENTITY_TYPE, stakedId, "user", USER_ADDRESS);
+  assert.fieldEquals(STAKED_TOKEN_ENTITY_TYPE, stakedId, "token", tokenId);
+  assert.fieldEquals(STAKED_TOKEN_ENTITY_TYPE, stakedId, "quantity", "1");
+
+  transferEvent = createTreasureTransferEvent(
+    ATLAS_MINE_ADDRESS,
+    USER_ADDRESS,
+    [95],
+    [1]
+  );
+
+  handleTransferBatch(transferEvent);
+
+  const unstakedEvent = createUnstakedEvent(
+    USER_ADDRESS,
+    TREASURE_ADDRESS,
+    95,
+    1,
+    0
+  );
+
+  handleUnstaked(unstakedEvent);
+
+  assert.fieldEquals(USER_ENTITY_TYPE, USER_ADDRESS, "boost", "0");
+  assert.fieldEquals(USER_ENTITY_TYPE, USER_ADDRESS, "boosts", "0");
+
+  assert.notInStore(STAKED_TOKEN_ENTITY_TYPE, stakedId);
+});
+
+test("stake/unstake handles when a multisig does the event", () => {
+  clearStore();
+
+  const mintEvent = createTreasureTransferEvent(
+    Address.zero().toHexString(),
+    USER_ADDRESS,
+    [95],
+    [1]
+  );
+
+  handleTransferBatch(mintEvent);
+
+  const transferToMultisigEvent = createTreasureTransferEvent(
+    USER_ADDRESS,
+    MULTISIG_ADDRESS,
+    [95],
+    [1]
+  );
+
+  handleTransferBatch(transferToMultisigEvent);
+
+  let transferEvent = createTreasureTransferEvent(
+    MULTISIG_ADDRESS,
+    ATLAS_MINE_ADDRESS,
+    [95],
+    [1]
+  );
+
+  handleTransferBatch(transferEvent);
+
+  const stakedEvent = createStakedEvent(
+    USER_ADDRESS,
+    TREASURE_ADDRESS,
+    95,
+    1,
+    0.157,
+    MULTISIG_ADDRESS
+  );
+
+  handleStaked(stakedEvent);
+
+  assert.fieldEquals(USER_ENTITY_TYPE, MULTISIG_ADDRESS, "boost", "0.157");
+  assert.fieldEquals(USER_ENTITY_TYPE, MULTISIG_ADDRESS, "boosts", "1");
+  assert.fieldEquals(USER_ENTITY_TYPE, USER_ADDRESS, "boost", "0");
+  assert.fieldEquals(USER_ENTITY_TYPE, USER_ADDRESS, "boosts", "0");
+
+  const tokenId = `${TREASURE_ADDRESS.toHexString()}-0x${(95).toString(16)}`;
+  const stakedId = `${MULTISIG_ADDRESS}-${tokenId}`;
+
+  assert.fieldEquals(
+    USER_ENTITY_TYPE,
+    MULTISIG_ADDRESS,
+    "staked",
+    `[${stakedId}]`
+  );
+  assert.fieldEquals(
+    STAKED_TOKEN_ENTITY_TYPE,
+    stakedId,
+    "user",
+    MULTISIG_ADDRESS
+  );
+  assert.fieldEquals(STAKED_TOKEN_ENTITY_TYPE, stakedId, "token", tokenId);
+  assert.fieldEquals(STAKED_TOKEN_ENTITY_TYPE, stakedId, "quantity", "1");
+
+  transferEvent = createTreasureTransferEvent(
+    ATLAS_MINE_ADDRESS,
+    MULTISIG_ADDRESS,
+    [95],
+    [1]
+  );
+
+  handleTransferBatch(transferEvent);
+
+  const unstakedEvent = createUnstakedEvent(
+    USER_ADDRESS,
+    TREASURE_ADDRESS,
+    95,
+    1,
+    0,
+    MULTISIG_ADDRESS
+  );
+
+  handleUnstaked(unstakedEvent);
+
+  assert.fieldEquals(USER_ENTITY_TYPE, MULTISIG_ADDRESS, "boost", "0");
+  assert.fieldEquals(USER_ENTITY_TYPE, MULTISIG_ADDRESS, "boosts", "0");
+
+  assert.notInStore(STAKED_TOKEN_ENTITY_TYPE, stakedId);
 });
