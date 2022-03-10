c22288befb6936e6bcc2d2f66c9e74bd4b54e8d1
Neil Kistner <neil.kistner@gmail.com>
2022-03-10 06:37:04 -0600

    Add test case

--- subgraphs/bridgeworld/tests/summoning.test.ts
+++ subgraphs/bridgeworld/tests/summoning.test.ts
@@ -8,6 +8,7 @@ import {
   SUMMONING_ADDRESS,
 } from "@treasure/constants";
 
+import { Summon } from "../generated/schema";
 import { handleLegionCreated, handleTransfer } from "../src/mappings/legion";
 import {
   handleRandomRequest,
@@ -267,4 +268,117 @@ test("summon fatigue is tracked and cleared properly", () => {
     "cooldown",
     "null"
   );
+});
+
+test("handles when a summon fails", () => {
+  clearStore();
+
+  const randomRequestEvent = createRandomRequestEvent(0, 1);
+  handleRandomRequest(randomRequestEvent);
+
+  const mintEvent = createLegionTransferEvent(
+    Address.zero().toHexString(),
+    USER_ADDRESS,
+    7
+  );
+
+  handleTransfer(mintEvent);
+
+  const legionCreatedEvent = createLegionCreatedEvent(USER_ADDRESS, 7, 0, 6, 2);
+
+  handleLegionCreated(legionCreatedEvent);
+
+  const summoningStartedEvent = createSummoningStartedEvent(
+    USER_ADDRESS,
+    7,
+    0,
+    1643659676
+  );
+  handleSummoningStarted(summoningStartedEvent);
+
+  const id = `${LEGION_ADDRESS.toHexString()}-0x7`;
+  assert.fieldEquals(TOKEN_ENTITY_TYPE, id, "category", "Legion");
+
+  const metadata = `${id}-metadata`;
+  assert.fieldEquals(LEGION_INFO_ENTITY_TYPE, metadata, "summons", "1");
+
+  const summon = `${summoningStartedEvent.address.toHexString()}-0x7`;
+  assert.fieldEquals(SUMMON_ENTITY_TYPE, summon, "token", id);
+  assert.fieldEquals(SUMMON_ENTITY_TYPE, summon, "status", "Idle");
+
+  createMockedFunction(
+    SUMMONING_ADDRESS,
+    "didSummoningSucceed",
+    "didSummoningSucceed(uint256):(bool,uint256)"
+  )
+    .withArgs([ethereum.Value.fromI32(7)])
+    .returns([
+      ethereum.Value.fromBoolean(false),
+      ethereum.Value.fromI32(1644659676),
+    ]);
+
+  const seededEvent = createRandomSeededEvent(1);
+
+  handleRandomSeeded(seededEvent);
+
+  assert.fieldEquals(SUMMONING_CIRCLE_ENTITY_TYPE, "only", "crafters", "0");
+  assert.fieldEquals(SUMMONING_CIRCLE_ENTITY_TYPE, "only", "summoners", "1");
+  assert.fieldEquals(
+    SUMMONING_CIRCLE_ENTITY_TYPE,
+    "only",
+    "successRate",
+    "0.5"
+  );
+
+  // const newMintEvent = createLegionTransferEvent(
+  //   Address.zero().toHexString(),
+  //   USER_ADDRESS,
+  //   1
+  // );
+  // handleTransfer(newMintEvent);
+
+  // const newLegionCreatedEvent = createLegionCreatedEvent(
+  //   USER_ADDRESS,
+  //   1,
+  //   1,
+  //   1,
+  //   4
+  // );
+  // handleLegionCreated(newLegionCreatedEvent);
+
+  const summoningFinishedEvent = createdSummoningFinishedEvent(
+    USER_ADDRESS,
+    7,
+    0,
+    0
+  );
+  handleSummoningFinished(summoningFinishedEvent);
+
+  // assert.fieldEquals(
+  //   SUMMON_ENTITY_TYPE,
+  //   `${summon}-0x0`,
+  //   "prismUsed",
+  //   `${CONSUMABLE_ADDRESS.toHexString()}-0x2`
+  // );
+  assert.fieldEquals(
+    SUMMON_ENTITY_TYPE,
+    `${summon}-0x0`,
+    "endTimestamp",
+    "1644659676000"
+  );
+  assert.fieldEquals(SUMMON_ENTITY_TYPE, `${summon}-0x0`, "success", "false");
+  assert.fieldEquals(SUMMON_ENTITY_TYPE, `${summon}-0x0`, "status", "Finished");
+
+  let summonEntity = Summon.load(`${summon}-0x0`);
+
+  if (summonEntity) {
+    assert.assertNull(summonEntity.resultToken);
+  } else {
+    // Fail
+    assert.booleanEquals(true, false);
+  }
+
+  assert.fieldEquals(SUMMONING_CIRCLE_ENTITY_TYPE, "only", "crafters", "0");
+  assert.fieldEquals(SUMMONING_CIRCLE_ENTITY_TYPE, "only", "summoners", "0");
+  assert.fieldEquals(SUMMONING_CIRCLE_ENTITY_TYPE, "only", "successRate", "1");
 });
