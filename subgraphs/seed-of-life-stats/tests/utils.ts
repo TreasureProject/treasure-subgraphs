0279db10b1ff701803dee2390af85ffe7a64a61e
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2022-04-22 10:14:52 -0700

    add seed of life stats subgraph

---
+++ subgraphs/seed-of-life-stats/tests/utils.ts
@@ -0,0 +1,10 @@
+export const GLOBAL_STAT_ENTITY_TYPE = "GlobalStat";
+export const LIFEFORM_ENTITY_TYPE = "_Lifeform";
+export const TREASURE_STATE_ENTITY_TYPE = "TreasureStat";
+
+export const USER_ADDRESS = "0x461950b159366edcd2bcbee8126d973ac49238e0";
+
+export const GOLD_COIN_TOKEN_ID = 92;
+export const DONKEY_TOKEN_ID = 114;
+
+export const toHexString = (value: i32): string => `0x${value.toString(16)}`;
