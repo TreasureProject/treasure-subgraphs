2c83c40719d9a1bc08bbfdd6a1d6d2e12a7ad567
insprac <insprac.dev@gmail.com>
2022-05-01 17:09:06 +1200

    Added end timestamp to handle play Treasure Triad

--- subgraphs/bridgeworld/src/mappings/advanced-questing.ts
+++ subgraphs/bridgeworld/src/mappings/advanced-questing.ts
@@ -132,6 +132,20 @@ export function handleTreasureTriadPlayed(event: TreasureTriadPlayed): void {
 
   quest.treasureTriadResult = result.id;
 
+  const advancedQuesting = AdvancedQuesting.bind(ADVANCED_QUESTING_ADDRESS);
+  const endTimeResult = advancedQuesting.try_endTimeForLegion(
+    event.params._legionId
+  );
+
+  if (!endTimeResult.reverted) {
+    quest.endTimestamp = endTimeResult.value.value0.times(BigInt.fromI32(1000));
+    quest.stasisHitCount = endTimeResult.value.value1;
+  } else {
+    log.error("[advanced-quest-triad] Failed to get endTime for legion: {}", [
+      quest.token,
+    ]);
+  }
+
   result.save();
   quest.save();
 }
