3300ba4b3110457547bc74d38b9833cd39c946db
insprac <33383194+insprac@users.noreply.github.com>
2022-05-15 10:06:53 +1200

    Renamed Enchanter to Enchanting and fixed treasure fragment category combos (#133)

--- subgraphs/bridgeworld/src/helpers/advanced-questing.ts
+++ subgraphs/bridgeworld/src/helpers/advanced-questing.ts
@@ -5,16 +5,18 @@ import { ADVANCED_QUESTING_ADDRESS } from "@treasure/constants";
 import { AdvancedQuesting } from "../../generated/Advanced Questing/AdvancedQuesting";
 import { AdvancedQuest } from "../../generated/schema";
 
-export function setQuestEndTime(quest: AdvancedQuest, legionId: BigInt): void {
+export function setQuestEndTime(
+  quest: AdvancedQuest,
+  legionId: BigInt
+): boolean {
   const advancedQuesting = AdvancedQuesting.bind(ADVANCED_QUESTING_ADDRESS);
   const endTimeResult = advancedQuesting.try_endTimeForLegion(legionId);
 
   if (!endTimeResult.reverted) {
     quest.endTimestamp = endTimeResult.value.value0.times(BigInt.fromI32(1000));
     quest.stasisHitCount = endTimeResult.value.value1;
+    return true;
   } else {
-    log.error("[advanced-quest-triad] Failed to get endTime for legion: {}", [
-      quest.token,
-    ]);
+    return false;
   }
 }
