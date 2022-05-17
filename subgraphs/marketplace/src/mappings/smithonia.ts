2e7c7131c17dc1c21f701f6ec5828ad18fd7f0d6
Neil Kistner <neil.kistner@gmail.com>
2022-05-17 08:20:37 -0500

    Add Smithonia Resources collection

---
+++ subgraphs/marketplace/src/mappings/smithonia.ts
@@ -0,0 +1,25 @@
+import { Transfer } from "../../generated/TreasureMarketplace/ERC721";
+import {
+  TransferBatch,
+  TransferSingle,
+} from "../../generated/TreasureMarketplace/ERC1155";
+import { createErc721Collection, createErc1155Collection } from "../helpers";
+import * as common from "../mapping";
+
+export function handleTransferSingle(event: TransferSingle): void {
+  createErc1155Collection(event.address, "Smithonia Resources");
+
+  common.handleTransferSingle(event);
+}
+
+export function handleTransferBatch(event: TransferBatch): void {
+  createErc1155Collection(event.address, "Smithonia Resources");
+
+  common.handleTransferBatch(event);
+}
+
+export function handleTransfer(event: Transfer): void {
+  createErc721Collection(event.address, "Smithonia Weapons");
+
+  common.handleTransfer721(event);
+}
