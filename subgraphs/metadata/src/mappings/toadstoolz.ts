90b376e35876930df45e3a3da260caf4ca9f68c9
Neil Kistner <neil.kistner@gmail.com>
2022-05-18 06:49:05 -0500

    Force refresh of Toadstoolz images for layering/blurring issues (#135)

--- subgraphs/metadata/src/mappings/toadstoolz.ts
+++ subgraphs/metadata/src/mappings/toadstoolz.ts
@@ -1,9 +1,11 @@
 import { Address, BigInt, Bytes, json } from "@graphprotocol/graph-ts";
 
 import { ERC721, Transfer } from "../../generated/Peek-A-Boo/ERC721";
-import { Collection, Token } from "../../generated/schema";
+import { Attribute, Collection, Token } from "../../generated/schema";
 import { decode } from "../helpers/base64";
+import { getTokenId } from "../helpers/ids";
 import { updateTokenMetadata } from "../helpers/metadata";
+import { getOrCreateCollection } from "../helpers/models";
 import { isMint } from "../helpers/utils";
 import * as common from "../mapping";
 
@@ -43,6 +45,35 @@ export function handleTransfer(event: Transfer): void {
     isMint(params.from),
     event.block.timestamp
   );
+
+  if (event.block.number.toString() == "11079257") {
+    // Reprocess 8890 toads
+    const collection = getOrCreateCollection(event.address);
+    const attributes = [
+      "0x09cae384c6626102abe47ff50588a1dbe8432174-rarity-1 of 1",
+      "0x09cae384c6626102abe47ff50588a1dbe8432174-rarity-common",
+    ];
+    const tokenIds: string[] = [];
+
+    for (let index = 0; index < attributes.length; index++) {
+      const attribute = Attribute.load(attributes[index]);
+
+      if (!attribute) {
+        continue;
+      }
+
+      const _tokenIds = attribute._tokenIds;
+
+      for (let idx = 0; idx < _tokenIds.length; idx++) {
+        tokenIds.push(
+          getTokenId(collection, BigInt.fromString(_tokenIds[idx]))
+        );
+      }
+    }
+
+    collection._missingMetadataTokens = tokenIds;
+    collection.save();
+  }
 
   common.handleTransfer(transfer, fetchTokenMetadata);
 }
