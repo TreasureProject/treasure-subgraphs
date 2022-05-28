bf6ee4a999b8ed3255251096beb4c9ad056101ce
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2022-05-27 17:26:48 -0700

    add kote items collections

--- subgraphs/metadata/src/mappings/kote.ts
+++ subgraphs/metadata/src/mappings/kote.ts
@@ -2,8 +2,14 @@ import { Address, BigInt, Bytes, json } from "@graphprotocol/graph-ts";
 
 import { KOTE_QUESTING_ADDRESS } from "@treasure/constants";
 
+import {
+  ERC1155,
+  TransferBatch,
+  TransferSingle,
+} from "../../generated/KOTE Rings/ERC1155";
 import { ERC721, Transfer } from "../../generated/KOTE Squires/ERC721";
 import { Collection, Token } from "../../generated/schema";
+import { getIpfsJson } from "../helpers/json";
 import { NormalizedAttribute, updateTokenMetadata } from "../helpers/metadata";
 import { getOrCreateCollection, getOrCreateToken } from "../helpers/models";
 import { isMint } from "../helpers/utils";
@@ -124,4 +130,68 @@ export function handleTransfer(event: Transfer): void {
   }
 
   common.handleTransfer(transfer, fetchTokenMetadata);
+}
+
+function fetchItemTokenMetadata(
+  collection: Collection,
+  token: Token,
+  timestamp: BigInt
+): void {
+  const tokenIdString = token.tokenId.toString();
+
+  token.name = `KOTE Item #${tokenIdString}`;
+
+  const contract = ERC1155.bind(Address.fromString(collection.id));
+  const tokenUri = contract.try_uri(token.tokenId);
+
+  if (!tokenUri.reverted) {
+    const data = getIpfsJson(tokenUri.value);
+
+    if (data) {
+      updateTokenMetadata(collection, token, data, timestamp);
+    } else {
+      collection._missingMetadataTokens =
+        collection._missingMetadataTokens.concat([token.id]);
+    }
+  }
+}
+
+export function handleTransferSingle(event: TransferSingle): void {
+  const params = event.params;
+
+  if (params.id.toI32() == 0) {
+    return;
+  }
+
+  const transfer = new common.TransferEvent(
+    event.address,
+    params.id,
+    isMint(params.from),
+    event.block.timestamp
+  );
+
+  common.handleTransfer(transfer, fetchItemTokenMetadata);
+}
+
+export function handleTransferBatch(event: TransferBatch): void {
+  const params = event.params;
+  const ids = params.ids;
+  const length = ids.length;
+
+  for (let index = 0; index < length; index++) {
+    const id = ids[index];
+
+    if (id.toI32() == 0) {
+      continue;
+    }
+
+    const transfer = new common.TransferEvent(
+      event.address,
+      ids[index],
+      isMint(params.from),
+      event.block.timestamp
+    );
+
+    common.handleTransfer(transfer, fetchItemTokenMetadata);
+  }
 }
