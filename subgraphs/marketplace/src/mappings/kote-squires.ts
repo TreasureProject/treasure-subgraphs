564c764028d3a409f5f1ef1d04b9c40131b29c7f
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2022-06-01 18:47:09 -0700

    add kote items collections (#143)

--- subgraphs/marketplace/src/mappings/kote-squires.ts
+++
@@ -1,9 +0,0 @@
-import { Transfer } from "../../generated/TreasureMarketplace/ERC721";
-import { createErc721Collection } from "../helpers";
-import { handleTransfer721 } from "../mapping";
-
-export function handleTransfer(event: Transfer): void {
-  createErc721Collection(event.address, "KOTE Squires");
-
-  handleTransfer721(event);
-}
