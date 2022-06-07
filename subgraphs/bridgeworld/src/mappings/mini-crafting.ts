d21e87b422a270070c6a213ddf509461d86de1d7
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2022-06-07 15:10:25 -0700

    add block number to mini craft because timestamp is not unique per group (#149)

--- subgraphs/bridgeworld/src/mappings/mini-crafting.ts
+++ subgraphs/bridgeworld/src/mappings/mini-crafting.ts
@@ -5,7 +5,6 @@ import { LegionInfo, MiniCraft, Outcome } from "../../generated/schema";
 import { getAddressId } from "../helpers";
 
 export function handleCraftingFinished(event: CraftingFinished): void {
-  const timestamp = event.block.timestamp;
   const params = event.params;
   const tokenId = params._legionId;
   const xpGained = params._cpGained;
@@ -13,7 +12,8 @@ export function handleCraftingFinished(event: CraftingFinished): void {
   // Save mini craft details
   const miniCraftId = `${event.transaction.hash.toHex()}-${event.logIndex.toString()}`;
   const miniCraft = new MiniCraft(miniCraftId);
-  miniCraft.timestamp = timestamp;
+  miniCraft.blockNumber = event.block.number;
+  miniCraft.timestamp = event.block.timestamp;
   miniCraft.tier = params._tier;
   miniCraft.token = getAddressId(LEGION_ADDRESS, tokenId);
   miniCraft.user = params._user.toHexString();
