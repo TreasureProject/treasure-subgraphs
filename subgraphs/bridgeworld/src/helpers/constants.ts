94718362f724473b753184b6a08808381f065476
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2022-06-29 15:51:19 -0700

    update consumable names

--- subgraphs/bridgeworld/src/helpers/constants.ts
+++ subgraphs/bridgeworld/src/helpers/constants.ts
@@ -1,4 +1,6 @@
 export const DIFFICULTY = ["Easy", "Medium", "Hard"];
+export const CONSUMABLE_IPFS =
+  "ipfs://Qmd1hsvPDWrxtnfUna3pQyfmChyAkMenuziHS1gszM34P8/Consumables";
 export const LEGION_IPFS =
   "ipfs://QmRXV7Pu2Y2CaeYGCSWJLKXnnRgDa3qndisPwGv9ZjQowt";
 export const LEGION_PFP_IPFS =
