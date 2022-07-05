cad7da15c9bf6d4181c1bc17235a38188505f207
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2022-07-05 09:55:03 -0700

    update to latest consumables ipfs hash

--- subgraphs/bridgeworld/src/helpers/constants.ts
+++ subgraphs/bridgeworld/src/helpers/constants.ts
@@ -1,6 +1,6 @@
 export const DIFFICULTY = ["Easy", "Medium", "Hard"];
 export const CONSUMABLE_IPFS =
-  "ipfs://Qmd1hsvPDWrxtnfUna3pQyfmChyAkMenuziHS1gszM34P8/Consumables";
+  "ipfs://Qma82rv8QoVuBkWUcovNcDbxHFaCEGrNHr5XtmdvKE4W98/Consumables";
 export const LEGION_IPFS =
   "ipfs://QmRXV7Pu2Y2CaeYGCSWJLKXnnRgDa3qndisPwGv9ZjQowt";
 export const LEGION_PFP_IPFS =
