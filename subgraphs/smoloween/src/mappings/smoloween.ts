807c6d824a3d7b771ff76ac83407bbfe88ea18de
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2022-09-15 17:30:30 -0700

    schema updates

--- subgraphs/smoloween/src/mappings/smoloween.ts
+++ subgraphs/smoloween/src/mappings/smoloween.ts
@@ -1,26 +1,18 @@
-import { Address, BigInt, log } from "@graphprotocol/graph-ts";
-
-import { SMOL_BODIES_ADDRESS } from "@treasure/constants";
+import { BigInt, log } from "@graphprotocol/graph-ts";
 
 import {
-  SmolStaked,
-  SmolUnstaked,
-} from "../../generated/Smol Racing/SmolRacing";
-import { Token } from "../../generated/schema";
+  Converted,
+  GotSidekick,
+  Staked,
+  Unstaked,
+} from "../../generated/Smoloween/Smoloween";
+import { Sidekick, Token } from "../../generated/schema";
 
-const getTokenType = (contract: Address): string =>
-  contract.equals(SMOL_BODIES_ADDRESS) ? "SmolBodies" : "SmolBrains";
-
-const getTokenId = (contract: Address, tokenId: BigInt): string =>
-  `${contract.toHexString()}-${tokenId.toString()}`;
-
-const getOrCreateToken = (contract: Address, tokenId: BigInt): Token => {
-  const id = getTokenId(contract, tokenId);
+const getOrCreateToken = (tokenId: BigInt): Token => {
+  const id = tokenId.toString();
   let token = Token.load(id);
   if (!token) {
     token = new Token(id);
-    token.type = getTokenType(contract);
-    token.contract = contract;
     token.tokenId = tokenId;
     token.save();
   }
@@ -28,23 +20,53 @@ const getOrCreateToken = (contract: Address, tokenId: BigInt): Token => {
   return token;
 };
 
-export function handleSmolStaked(event: SmolStaked): void {
+export function handleStaked(event: Staked): void {
   const params = event.params;
-  const token = getOrCreateToken(params._smolAddress, params._tokenId);
+  const token = getOrCreateToken(params.smolId);
   token.isStaked = true;
   token.save();
 }
 
-export function handleSmolUnstaked(event: SmolUnstaked): void {
+export function handleUnstaked(event: Unstaked): void {
   const params = event.params;
 
-  const id = getTokenId(params._smolAddress, params._tokenId);
+  const id = params.id.toString();
   const token = Token.load(id);
   if (!token) {
-    log.error("[smol-racing] Unstaking unknown Token: {}", [id]);
+    log.error("[smoloween] Unstaking unknown Token: {}", [id]);
     return;
   }
 
   token.isStaked = false;
   token.save();
+}
+
+export function handleConverted(event: Converted): void {
+  const params = event.params;
+
+  const id = params.smol.toString();
+  const token = Token.load(id);
+  if (!token) {
+    log.error("[smoloween] Converting unknown Token: {}", [id]);
+    return;
+  }
+
+  token.isGhoul = true;
+  token.save();
+}
+
+export function handleGotSidekick(event: GotSidekick): void {
+  const params = event.params;
+
+  const ownerId = params.smol.toString();
+  const owner = Token.load(ownerId);
+  if (!owner) {
+    log.error("[smoloween] Adding sidekick to unknown Token: {}", [ownerId]);
+    return;
+  }
+
+  const sidekick = new Sidekick(params.sidekick.toString());
+  sidekick.owner = owner.id;
+  sidekick.day = params.day.toI32();
+  sidekick.save();
 }
