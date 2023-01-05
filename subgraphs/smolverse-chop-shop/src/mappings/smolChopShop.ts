6c6f735626e1012bb73443aed76344ab6571efce
Cristian Dotta <cdotta90@gmail.com>
2023-01-05 14:07:05 -0300

    Add support for upgrade updates

--- subgraphs/smolverse-chop-shop/src/mappings/smolChopShop.ts
+++ subgraphs/smolverse-chop-shop/src/mappings/smolChopShop.ts
@@ -1,34 +1,84 @@
+import { Bytes, log } from "@graphprotocol/graph-ts";
+
 import {
   UpgradeAddedToContract,
   UpgradeAddedToContract_upgradeInfoStruct,
+  UpgradeInfoChanged,
+  UpgradeInfoChanged_upgradeInfoStruct,
+  UpgradeSaleStateChanged,
 } from "../../generated/SmolChopShop/SmolChopShop";
 import { Upgrade } from "../../generated/schema";
 
-function populateUpgradeFields(
-  upgrade: Upgrade,
-  upgradeInfo: UpgradeAddedToContract_upgradeInfoStruct
-): void {
-  upgrade.amountClaimed = upgradeInfo.amountClaimed;
-  upgrade.limitedOfferId = upgradeInfo.limitedOfferId;
-  upgrade.maxSupply = upgradeInfo.maxSupply;
-  upgrade.price = upgradeInfo.price;
-  upgrade.subgroupId = upgradeInfo.subgroupId;
-  upgrade.forSale = upgradeInfo.forSale;
-  upgrade.tradable = upgradeInfo.tradable;
-  upgrade.uncappedSupply = upgradeInfo.uncappedSupply;
-  upgrade.upgradeType = upgradeInfo.upgradeType;
-  upgrade.validSkinId = upgradeInfo.validSkinId;
-  upgrade.validVehicleType = upgradeInfo.validVehicleType;
-  upgrade.name = upgradeInfo.name;
-  upgrade.uri = upgradeInfo.uri;
-  upgrade.merkleRoot = upgradeInfo.merkleRoot.toString();
+function populateUpgradeFields<T>(upgrade: Upgrade, upgradeInfo: T): void {
+  if (
+    upgradeInfo instanceof UpgradeAddedToContract_upgradeInfoStruct ||
+    upgradeInfo instanceof UpgradeInfoChanged_upgradeInfoStruct
+  ) {
+    upgrade.amountClaimed = upgradeInfo.amountClaimed;
+    upgrade.limitedOfferId = upgradeInfo.limitedOfferId;
+    upgrade.maxSupply = upgradeInfo.maxSupply;
+    upgrade.price = upgradeInfo.price;
+    upgrade.subgroupId = upgradeInfo.subgroupId;
+    upgrade.forSale = upgradeInfo.forSale;
+    upgrade.tradable = upgradeInfo.tradable;
+    upgrade.uncappedSupply = upgradeInfo.uncappedSupply;
+    upgrade.upgradeType = upgradeInfo.upgradeType;
+    upgrade.validSkinId = upgradeInfo.validSkinId;
+    upgrade.validVehicleType = upgradeInfo.validVehicleType;
+    upgrade.name = upgradeInfo.name;
+    upgrade.uri = upgradeInfo.uri;
+    upgrade.merkleRoot = upgradeInfo.merkleRoot.toString();
+  }
 }
 
 export function handleNewUpgrade(event: UpgradeAddedToContract): void {
   const params = event.params;
-  const upgrade = new Upgrade(params._upgradeId.toString());
+  const id = Bytes.fromI32(params._upgradeId.toI32());
+  const upgrade = new Upgrade(id);
 
-  populateUpgradeFields(upgrade, params._upgradeInfo);
+  populateUpgradeFields<UpgradeAddedToContract_upgradeInfoStruct>(
+    upgrade,
+    params._upgradeInfo
+  );
+
+  upgrade.save();
+}
+
+export function handleUpgradeUpdate(event: UpgradeInfoChanged): void {
+  const params = event.params;
+  const id = Bytes.fromI32(params._upgradeId.toI32());
+  const upgrade = Upgrade.load(id);
+
+  if (!upgrade) {
+    log.error("[smolverse-chop-shop] Updating unknown Upgrade: {}", [
+      params._upgradeId.toString(),
+    ]);
+    return;
+  }
+
+  populateUpgradeFields<UpgradeInfoChanged_upgradeInfoStruct>(
+    upgrade,
+    params._upgradeInfo
+  );
+
+  upgrade.save();
+}
+
+export function handleUpgradeSaleStateUpdate(
+  event: UpgradeSaleStateChanged
+): void {
+  const params = event.params;
+  const id = Bytes.fromI32(params._upgradeId.toI32());
+  const upgrade = Upgrade.load(id);
+
+  if (!upgrade) {
+    log.error("[smolverse-chop-shop] Updating unknown Upgrade: {}", [
+      params._upgradeId.toString(),
+    ]);
+    return;
+  }
+
+  upgrade.forSale = params._added;
 
   upgrade.save();
 }
