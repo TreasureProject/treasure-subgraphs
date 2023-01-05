170d225886b9868d45f1fda085d18e1fc83d00dc
Cristian Dotta <cdotta90@gmail.com>
2023-01-05 12:02:54 -0300

    Add chop-shop subgraph

---
+++ subgraphs/smolverse-chop-shop/src/mappings/smolChopShop.ts
@@ -0,0 +1,33 @@
+import {
+  UpgradeAddedToContract,
+  UpgradeAddedToContract_upgradeInfoStruct,
+} from "../../generated/SmolChopShop/SmolChopShop";
+import { Upgrade } from "../../generated/schema";
+
+function populateUpgradeFields(
+  upgrade: Upgrade,
+  upgradeInfo: UpgradeAddedToContract_upgradeInfoStruct
+) {
+  upgrade.amountClaimed = upgradeInfo.amountClaimed;
+  upgrade.limitedOfferId = upgradeInfo.limitedOfferId;
+  upgrade.maxSupply = upgradeInfo.maxSupply;
+  upgrade.price = upgradeInfo.price;
+  upgrade.subgroupId = upgradeInfo.subgroupId;
+  upgrade.forSale = upgradeInfo.forSale;
+  upgrade.tradable = upgradeInfo.tradable;
+  upgrade.uncappedSupply = upgradeInfo.uncappedSupply;
+  upgrade.upgradeType = upgradeInfo.upgradeType;
+  upgrade.validSkinId = upgradeInfo.validSkinId;
+  upgrade.upgradeType = upgradeInfo.upgradeType;
+  upgrade.name = upgradeInfo.name;
+  upgrade.uri = upgradeInfo.uri;
+  upgrade.merkleRoot = upgradeInfo.merkleRoot.toString();
+}
+
+export function handleNewUpgrade({ params }: UpgradeAddedToContract) {
+  const upgrade = new Upgrade(params._upgradeId.toString());
+
+  populateUpgradeFields(upgrade, params._upgradeInfo);
+
+  upgrade.save();
+}
