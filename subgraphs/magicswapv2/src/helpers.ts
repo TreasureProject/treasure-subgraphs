cd0fc485f06b69e604db6b4f364224aa4355eb59
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2023-03-18 22:58:20 -0700

    add deposit and withdraw handlers

--- subgraphs/magicswapv2/src/helpers.ts
+++ subgraphs/magicswapv2/src/helpers.ts
@@ -1,7 +1,7 @@
 import { Address, BigInt, log } from "@graphprotocol/graph-ts";
 
 import { ERC20 } from "../generated/UniswapV2Factory/ERC20";
-import { Collection, NftVault, Token } from "../generated/schema";
+import { Collection, Token } from "../generated/schema";
 import { ZERO_BD, ZERO_BI } from "./const";
 import { exponentToBigDecimal } from "./utils";
 
@@ -21,38 +21,54 @@ export const getOrCreateCollection = (
   return collection;
 };
 
+export const setTokenContractData = (token: Token): void => {
+  const address = Address.fromBytes(token.id);
+  const contract = ERC20.bind(address);
+
+  if (!token.name) {
+    const nameResult = contract.try_name();
+    if (nameResult.reverted) {
+      log.error("Error reading token name: {}", [address.toHexString()]);
+      token.name = "";
+    } else {
+      token.name = nameResult.value;
+    }
+  }
+
+  if (!token.symbol) {
+    const symbolResult = contract.try_symbol();
+    if (symbolResult.reverted) {
+      log.error("Error reading token symbol: {}", [address.toHexString()]);
+      token.symbol = "";
+    } else {
+      token.symbol = symbolResult.value;
+    }
+  }
+
+  const totalSupplyResult = contract.try_totalSupply();
+  if (totalSupplyResult.reverted) {
+    log.error("Error reading token total supply: {}", [address.toHexString()]);
+    token.totalSupply = ZERO_BI;
+  } else {
+    token.totalSupply = totalSupplyResult.value;
+  }
+
+  const decimalsResult = contract.try_decimals();
+  if (decimalsResult.reverted) {
+    log.error("Error reading token decimals: {}", [address.toHexString()]);
+    token.decimals = ZERO_BI;
+    token.decimalDivisor = ZERO_BD;
+  } else {
+    token.decimals = BigInt.fromI32(decimalsResult.value);
+    token.decimalDivisor = exponentToBigDecimal(token.decimals);
+  }
+};
+
 export const getOrCreateToken = (address: Address): Token => {
   let token = Token.load(address);
   if (!token) {
     token = new Token(address);
-
-    const nftVault = NftVault.load(address);
-    if (nftVault) {
-      token.nftVault = nftVault.id;
-    }
-
-    const contract = ERC20.bind(address);
-
-    const totalSupplyResult = contract.try_totalSupply();
-    if (totalSupplyResult.reverted) {
-      log.error("Error reading token total supply: {}", [
-        address.toHexString(),
-      ]);
-      token.totalSupply = ZERO_BI;
-    } else {
-      token.totalSupply = totalSupplyResult.value;
-    }
-
-    const decimalsResult = contract.try_decimals();
-    if (decimalsResult.reverted) {
-      log.error("Error reading token decimals: {}", [address.toHexString()]);
-      token.decimals = ZERO_BI;
-      token.decimalDivisor = ZERO_BD;
-    } else {
-      token.decimals = BigInt.fromI32(decimalsResult.value);
-      token.decimalDivisor = exponentToBigDecimal(token.decimals);
-    }
-
+    setTokenContractData(token);
     token.save();
   }
 
---
+++ subgraphs/magicswapv2/src/mappings/vault.ts
@@ -0,0 +1,101 @@
+import { log, store } from "@graphprotocol/graph-ts";
+
+import { VaultCreated } from "../../generated/NftVaultFactory/NftVaultFactory";
+import {
+  Deposit,
+  Token,
+  VaultCollection,
+  VaultReserveItem,
+  Withdrawal,
+} from "../../generated/schema";
+import {
+  Deposit as DepositEvent,
+  Withdraw,
+} from "../../generated/templates/NftVault/NftVault";
+import { getOrCreateCollection, setTokenContractData } from "../helpers";
+
+export function handleVaultCreated(event: VaultCreated): void {
+  const params = event.params;
+
+  const vault = new Token(params.vault);
+  vault.name = params.name;
+  vault.symbol = params.symbol;
+  setTokenContractData(vault);
+  vault.save();
+
+  for (let i = 0; i < params.collections.length; i++) {
+    const collection = getOrCreateCollection(
+      params.collections[i].addr,
+      params.collections[i].nftType
+    );
+    const vaultCollection = new VaultCollection(vault.id.concatI32(i));
+    vaultCollection.vault = vault.id;
+    vaultCollection.collection = collection.id;
+    if (!params.collections[i].allowAllIds) {
+      vaultCollection.tokenIds = params.collections[i].tokenIds;
+    }
+    vaultCollection.save();
+  }
+}
+
+export function handleDeposit(event: DepositEvent): void {
+  const params = event.params;
+
+  const deposit = new Deposit(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  deposit.transactionHash = event.transaction.hash;
+  deposit.timestamp = event.block.timestamp;
+  deposit.vault = event.address;
+  deposit.collection = params.collection;
+  deposit.tokenId = params.tokenId;
+  deposit.amount = params.amount.toI32();
+  deposit.save();
+
+  const reserveItemId = event.address
+    .concat(params.collection)
+    .concatI32(params.tokenId.toI32());
+  let reserveItem = VaultReserveItem.load(reserveItemId);
+  if (!reserveItem) {
+    reserveItem = new VaultReserveItem(reserveItemId);
+    reserveItem.vault = event.address;
+    reserveItem.collection = params.collection;
+    reserveItem.tokenId = params.tokenId;
+    reserveItem.amount = 0;
+  }
+
+  reserveItem.amount += params.amount.toI32();
+  reserveItem.save();
+}
+
+export function handleWithdraw(event: Withdraw): void {
+  const params = event.params;
+
+  const withdrawal = new Withdrawal(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  withdrawal.transactionHash = event.transaction.hash;
+  withdrawal.timestamp = event.block.timestamp;
+  withdrawal.vault = event.address;
+  withdrawal.collection = params.collection;
+  withdrawal.tokenId = params.tokenId;
+  withdrawal.amount = params.amount.toI32();
+  withdrawal.save();
+
+  const reserveItem = VaultReserveItem.load(
+    event.address.concat(params.collection).concatI32(params.tokenId.toI32())
+  );
+  if (!reserveItem) {
+    log.warning("Withdrawing unknown reserve item: {}", [
+      event.transaction.hash.toHexString(),
+    ]);
+    return;
+  }
+
+  reserveItem.amount -= params.amount.toI32();
+  if (reserveItem.amount > 0) {
+    reserveItem.save();
+  } else {
+    store.remove("VaultReserveItem", reserveItem.id.toHexString());
+  }
+}
