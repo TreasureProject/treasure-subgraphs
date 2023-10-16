75965a8c5242eb5106776a07a5e51946c662c4f5
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2023-10-16 14:20:17 -0700

    add smol trait dependency levels

--- subgraphs/smols-traits/src/mapping.ts
+++ subgraphs/smols-traits/src/mapping.ts
@@ -1,7 +1,7 @@
 import { Bytes } from "@graphprotocol/graph-ts";
 
 import { TraitAdded } from "../generated/Smols Trait Storage/SmolsTraitStorage";
-import { Trait } from "../generated/schema";
+import { Trait, TraitDependency } from "../generated/schema";
 
 const GENDERS = ["Unset", "Male", "Female"];
 
@@ -18,8 +18,20 @@ export function handleTraitAdded(event: TraitAdded): void {
   trait.gender = GENDERS[params._trait.gender];
   trait.traitName = params._trait.traitName.toString();
   trait.traitType = params._trait.traitType.toString();
-  trait.maleImage = params._trait.pngImage.male.toString();
-  trait.femaleImage = params._trait.pngImage.female.toString();
   trait.isDetachable = params._trait.isDetachable;
   trait.save();
+
+  const dependencyId = Bytes.fromI32(params._traitId.toI32()).concatI32(
+    params._dependencyLevel.toI32()
+  );
+  let traitDependency = TraitDependency.load(dependencyId);
+  if (!traitDependency) {
+    traitDependency = new TraitDependency(dependencyId);
+  }
+
+  traitDependency.trait = id;
+  traitDependency.dependencyLevel = params._dependencyLevel.toI32();
+  traitDependency.maleImage = params._trait.pngImage.male.toString();
+  traitDependency.femaleImage = params._trait.pngImage.female.toString();
+  traitDependency.save();
 }
