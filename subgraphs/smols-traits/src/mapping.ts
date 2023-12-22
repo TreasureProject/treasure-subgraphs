de29b94be6c42b10758affe309c735d3c299ba05
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2023-12-21 16:32:49 -0800

    handle update and delete events

--- subgraphs/smols-traits/src/mapping.ts
+++ subgraphs/smols-traits/src/mapping.ts
@@ -1,7 +1,11 @@
-import { BigInt, Bytes } from "@graphprotocol/graph-ts";
+import { BigInt, Bytes, store } from "@graphprotocol/graph-ts";
 
 import { TraitAdded } from "../generated/Smols Trait Storage/SmolsTraitStorage";
-import { SmolRecipeAdded } from "../generated/Transmolgrifier/Transmolgrifier";
+import {
+  SmolRecipeAdded,
+  SmolRecipeAdjusted,
+  SmolRecipeDeleted,
+} from "../generated/Transmolgrifier/Transmolgrifier";
 import { Recipe, Season, Trait, TraitDependency } from "../generated/schema";
 
 const GENDERS = ["Unset", "Male", "Female"];
@@ -81,4 +85,35 @@ export function handleRecipeAdded(event: SmolRecipeAdded): void {
   recipe.treasureCost = data.treasureAmount;
   recipe.treasureTokenId = data.treasureId;
   recipe.save();
+}
+
+export function handleRecipeAdjusted(event: SmolRecipeAdjusted): void {
+  const params = event.params;
+  const data = params.smolData;
+  const smol = data.smol;
+
+  const recipe = getOrCreateRecipe(params.seasonId, params.smolRecipeId);
+  recipe.background =
+    smol.background > 0 ? Bytes.fromI32(smol.background) : null;
+  recipe.body = smol.body > 0 ? Bytes.fromI32(smol.body) : null;
+  recipe.clothes = smol.clothes > 0 ? Bytes.fromI32(smol.clothes) : null;
+  recipe.mouth = smol.mouth > 0 ? Bytes.fromI32(smol.mouth) : null;
+  recipe.glasses = smol.glasses > 0 ? Bytes.fromI32(smol.glasses) : null;
+  recipe.hat = smol.hat > 0 ? Bytes.fromI32(smol.hat) : null;
+  recipe.hair = smol.hair > 0 ? Bytes.fromI32(smol.hair) : null;
+  recipe.skin = smol.skin > 0 ? Bytes.fromI32(smol.skin) : null;
+  recipe.smolCost = data.smolInputAmount;
+  recipe.treasureCost = data.treasureAmount;
+  recipe.treasureTokenId = data.treasureId;
+  recipe.save();
+}
+
+export function handleRecipeDeleted(event: SmolRecipeDeleted): void {
+  const params = event.params;
+  store.remove(
+    "Recipe",
+    Bytes.fromI32(params.seasonId.toI32())
+      .concatI32(params.smolRecipeId.toI32())
+      .toHexString()
+  );
 }
