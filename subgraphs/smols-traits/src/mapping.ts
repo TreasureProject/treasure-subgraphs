97e2785d7a26a613e2238c139bb0fa556233c4ce
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-01-28 21:17:31 -0800

    rerender smol recipes on trait changes

--- subgraphs/smols-traits/src/mapping.ts
+++ subgraphs/smols-traits/src/mapping.ts
@@ -64,6 +64,24 @@ export function handleTraitAdded(event: TraitAdded): void {
   trait.isDetachable = params._trait.isDetachable;
   trait.save();
 
+  if (trait.traitType == "Background") {
+    renderRecipes(trait.backgroundRecipes.load());
+  } else if (trait.traitType == "Body") {
+    renderRecipes(trait.bodyRecipes.load());
+  } else if (trait.traitType == "Clothes") {
+    renderRecipes(trait.clothesRecipes.load());
+  } else if (trait.traitType == "Mouth") {
+    renderRecipes(trait.mouthRecipes.load());
+  } else if (trait.traitType == "Glasses") {
+    renderRecipes(trait.glassesRecipes.load());
+  } else if (trait.traitType == "Hat") {
+    renderRecipes(trait.hatRecipes.load());
+  } else if (trait.traitType == "Hair") {
+    renderRecipes(trait.hairRecipes.load());
+  } else if (trait.traitType == "Skin") {
+    renderRecipes(trait.skinRecipes.load());
+  }
+
   const dependencyId = Bytes.fromI32(params._traitId.toI32()).concatI32(
     params._dependencyLevel.toI32()
   );
@@ -78,6 +96,51 @@ export function handleTraitAdded(event: TraitAdded): void {
   traitDependency.femaleImage = params._trait.pngImage.female.toString();
   traitDependency.save();
 }
+
+const renderRecipe = (recipe: Recipe): void => {
+  const contract = SmolRenderer.bind(SMOL_RENDERER_ADDRESS);
+  const tuple = new SmolRenderer__generateSVGInput_smolStruct(10);
+  tuple[0] = ethereum.Value.fromUnsignedBigInt(
+    BigInt.fromI32(recipe.background ? (recipe.background as Bytes).toI32() : 0)
+  );
+  tuple[1] = ethereum.Value.fromUnsignedBigInt(
+    BigInt.fromI32(recipe.body ? (recipe.body as Bytes).toI32() : 0)
+  );
+  tuple[2] = ethereum.Value.fromUnsignedBigInt(
+    BigInt.fromI32(recipe.clothes ? (recipe.clothes as Bytes).toI32() : 0)
+  );
+  tuple[3] = ethereum.Value.fromUnsignedBigInt(
+    BigInt.fromI32(recipe.mouth ? (recipe.mouth as Bytes).toI32() : 0)
+  );
+  tuple[4] = ethereum.Value.fromUnsignedBigInt(
+    BigInt.fromI32(recipe.glasses ? (recipe.glasses as Bytes).toI32() : 0)
+  );
+  tuple[5] = ethereum.Value.fromUnsignedBigInt(
+    BigInt.fromI32(recipe.hat ? (recipe.hat as Bytes).toI32() : 0)
+  );
+  tuple[6] = ethereum.Value.fromUnsignedBigInt(
+    BigInt.fromI32(recipe.hair ? (recipe.hair as Bytes).toI32() : 0)
+  );
+  tuple[7] = ethereum.Value.fromUnsignedBigInt(
+    BigInt.fromI32(recipe.skin ? (recipe.skin as Bytes).toI32() : 0)
+  );
+  tuple[8] = ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(recipe.gender));
+  tuple[9] = ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(recipe.headSize));
+  const result = contract.try_generateSVG(tuple);
+  if (!result.reverted) {
+    recipe.image = result.value.toString();
+  } else {
+    log.warning("Error rendering recipe: {}", [recipe.recipeId.toString()]);
+    recipe.image = "";
+  }
+};
+
+const renderRecipes = (recipes: Recipe[]): void => {
+  for (let i = 0; i < recipes.length; i += 1) {
+    renderRecipe(recipes[i]);
+    recipes[i].save();
+  }
+};
 
 const updateRecipe = (
   seasonId: BigInt,
@@ -110,27 +173,7 @@ const updateRecipe = (
   recipe.smolCost = smolCost;
   recipe.treasureCost = treasureCost;
   recipe.treasureTokenId = treasureTokenId;
-
-  const contract = SmolRenderer.bind(SMOL_RENDERER_ADDRESS);
-  const tuple = new SmolRenderer__generateSVGInput_smolStruct(10);
-  tuple[0] = ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(background));
-  tuple[1] = ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(body));
-  tuple[2] = ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(clothes));
-  tuple[3] = ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(mouth));
-  tuple[4] = ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(glasses));
-  tuple[5] = ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(hat));
-  tuple[6] = ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(hair));
-  tuple[7] = ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(skin));
-  tuple[8] = ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(gender));
-  tuple[9] = ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(headSize));
-  const result = contract.try_generateSVG(tuple);
-  if (!result.reverted) {
-    recipe.image = result.value.toString();
-  } else {
-    log.warning("Error rendering recipe: {}", [recipeId.toString()]);
-    recipe.image = "";
-  }
-
+  renderRecipe(recipe);
   recipe.save();
 };
 
