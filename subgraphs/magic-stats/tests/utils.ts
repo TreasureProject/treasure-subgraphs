116a94b260063772b4d4240188123e32cd726350
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-02-05 18:21:03 -0800

    remove deprecated subgraphs; migrate to npm; switch deploy flow to goldsky (#226)
    * remove deprecated subgraphs
    
    * rename smol subgraph
    
    * remove unused common package
    
    * add abis to magicswap-exchange
    
    * add abis to bridgeworld-recruits
    
    * add abis to bridgeworld-kote
    
    * add abis to bridgeworld-corruption
    
    * add abis to bridgeworld-approvals
    
    * add abis to bridgeworld
    
    * migrate to npm
    
    * upgrade husky
    
    * upgrade matchstick-as
    
    * switch deploy to goldsky
    
    * use goldsky npm package
    
    * run npm install in root context
    
    * fix yaml formatting
    
    * add build step
    
    * revert to matchstick 0.5.4

--- subgraphs/magic-stats/tests/utils.ts
+++
@@ -1,8 +0,0 @@
-import { Address } from "@graphprotocol/graph-ts";
-
-export const MAGIC_STAT_ENTITY_TYPE = "MagicStat";
-export const USER_STAT_ENTITY_TYPE = "UserStat";
-
-export const USER_ADDRESS = "0x461950b159366edcd2bcbee8126d973ac49238e0";
-export const USER_ADDRESS2 = "0x461950b159366edcd2bcbee8126d973ac49238e1";
-export const ZERO_ADDRESS = Address.zero().toHexString();
--- subgraphs/smolverse/src/helpers/utils.ts
+++
@@ -1,5 +0,0 @@
-import { Address } from "@graphprotocol/graph-ts";
-
-export function isZero(address: Address): bool {
-  return address.equals(Address.zero());
-}
