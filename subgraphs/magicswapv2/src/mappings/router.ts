2ac7717ee1c5ab13a8a438b0acf8a79fca907000
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-07-14 17:09:47 -0700

    handle nft-nft liquidity events

--- subgraphs/magicswapv2/src/mappings/router.ts
+++ subgraphs/magicswapv2/src/mappings/router.ts
@@ -24,11 +24,11 @@ export function handleNftLiquidityAdded(event: NFTLiquidityAdded): void {
     return;
   }
 
+  const vault = params.vault.token;
   let items0: Bytes[] = [];
   let items1: Bytes[] = [];
 
   for (let i = 0; i < params.vault.collection.length; i += 1) {
-    const vault = params.vault.token;
     const collection = params.vault.collection[i];
     const tokenId = params.vault.tokenId[i];
     const amount = params.vault.amount[i].toI32();
@@ -75,11 +75,11 @@ export function handleNftLiquidityRemoved(event: NFTLiquidityRemoved): void {
     return;
   }
 
+  const vault = params.vault.token;
   let items0: Bytes[] = [];
   let items1: Bytes[] = [];
 
   for (let i = 0; i < params.vault.collection.length; i += 1) {
-    const vault = params.vault.token;
     const collection = params.vault.collection[i];
     const tokenId = params.vault.tokenId[i];
     const amount = params.vault.amount[i].toI32();
@@ -120,17 +120,187 @@ export function handleNftLiquidityRemoved(event: NFTLiquidityRemoved): void {
 }
 
 export function handleNftNftLiquidityAdded(event: NFTNFTLiquidityAdded): void {
+  const params = event.params;
+
+  const pair = Pair.load(params.pair);
+  if (!pair) {
+    log.error("Error adding NFT liquidity to unknown Pair: {}", [
+      params.pair.toHexString(),
+    ]);
+    return;
+  }
+
+  const vaultA = params.vaultA.token;
+  const vaultB = params.vaultB.token;
+  let items0: Bytes[] = [];
+  let items1: Bytes[] = [];
+
+  for (let i = 0; i < params.vaultA.collection.length; i += 1) {
+    const collection = params.vaultA.collection[i];
+    const tokenId = params.vaultA.tokenId[i];
+    const amount = params.vaultA.amount[i].toI32();
+
+    const reserveItem = getOrCreateVaultReserveItem(
+      vaultA,
+      collection,
+      tokenId
+    );
+    reserveItem.amount += amount;
+    reserveItem.save();
+
+    const transactionItem = new TransactionItem(
+      event.transaction.hash
+        .concat(vaultA)
+        .concat(collection)
+        .concatI32(tokenId.toI32())
+    );
+    transactionItem.vault = vaultA;
+    transactionItem.collection = collection;
+    transactionItem.tokenId = tokenId;
+    transactionItem.amount = amount;
+    transactionItem.save();
+
+    if (vaultA.equals(pair.token0)) {
+      items0.push(transactionItem.id);
+    } else if (vaultA.equals(pair.token1)) {
+      items1.push(transactionItem.id);
+    }
+  }
+
+  for (let i = 0; i < params.vaultB.collection.length; i += 1) {
+    const collection = params.vaultB.collection[i];
+    const tokenId = params.vaultB.tokenId[i];
+    const amount = params.vaultB.amount[i].toI32();
+
+    const reserveItem = getOrCreateVaultReserveItem(
+      vaultB,
+      collection,
+      tokenId
+    );
+    reserveItem.amount += amount;
+    reserveItem.save();
+
+    const transactionItem = new TransactionItem(
+      event.transaction.hash
+        .concat(vaultB)
+        .concat(collection)
+        .concatI32(tokenId.toI32())
+    );
+    transactionItem.vault = vaultB;
+    transactionItem.collection = collection;
+    transactionItem.tokenId = tokenId;
+    transactionItem.amount = amount;
+    transactionItem.save();
+
+    if (vaultB.equals(pair.token0)) {
+      items0.push(transactionItem.id);
+    } else if (vaultB.equals(pair.token1)) {
+      items1.push(transactionItem.id);
+    }
+  }
+
   const transaction = getOrCreateTransaction(event);
   transaction.type = "Deposit";
-  transaction.user = getOrCreateUser(event.params.to).id;
+  transaction.user = getOrCreateUser(params.to).id;
+  transaction.items0 = items0;
+  transaction.items1 = items1;
   transaction.save();
 }
 
 export function handleNftNftLiquidityRemoved(
   event: NFTNFTLiquidityRemoved
 ): void {
+  const params = event.params;
+
+  const pair = Pair.load(params.pair);
+  if (!pair) {
+    log.error("Error removing NFT liquidity from unknown Pair: {}", [
+      params.pair.toHexString(),
+    ]);
+    return;
+  }
+
+  const vaultA = params.vaultA.token;
+  const vaultB = params.vaultB.token;
+  let items0: Bytes[] = [];
+  let items1: Bytes[] = [];
+
+  for (let i = 0; i < params.vaultA.collection.length; i += 1) {
+    const collection = params.vaultA.collection[i];
+    const tokenId = params.vaultA.tokenId[i];
+    const amount = params.vaultA.amount[i].toI32();
+
+    const reserveItem = getOrCreateVaultReserveItem(
+      vaultA,
+      collection,
+      tokenId
+    );
+    reserveItem.amount -= amount;
+    if (reserveItem.amount > 0) {
+      reserveItem.save();
+    } else {
+      store.remove("VaultReserveItem", reserveItem.id.toHexString());
+    }
+
+    const transactionItem = new TransactionItem(
+      event.transaction.hash
+        .concat(vaultA)
+        .concat(collection)
+        .concatI32(tokenId.toI32())
+    );
+    transactionItem.vault = vaultA;
+    transactionItem.collection = collection;
+    transactionItem.tokenId = tokenId;
+    transactionItem.amount = amount;
+    transactionItem.save();
+
+    if (vaultA.equals(pair.token0)) {
+      items0.push(transactionItem.id);
+    } else if (vaultA.equals(pair.token1)) {
+      items1.push(transactionItem.id);
+    }
+  }
+
+  for (let i = 0; i < params.vaultB.collection.length; i += 1) {
+    const collection = params.vaultB.collection[i];
+    const tokenId = params.vaultB.tokenId[i];
+    const amount = params.vaultB.amount[i].toI32();
+
+    const reserveItem = getOrCreateVaultReserveItem(
+      vaultB,
+      collection,
+      tokenId
+    );
+    reserveItem.amount -= amount;
+    if (reserveItem.amount > 0) {
+      reserveItem.save();
+    } else {
+      store.remove("VaultReserveItem", reserveItem.id.toHexString());
+    }
+
+    const transactionItem = new TransactionItem(
+      event.transaction.hash
+        .concat(vaultB)
+        .concat(collection)
+        .concatI32(tokenId.toI32())
+    );
+    transactionItem.vault = vaultB;
+    transactionItem.collection = collection;
+    transactionItem.tokenId = tokenId;
+    transactionItem.amount = amount;
+    transactionItem.save();
+
+    if (vaultB.equals(pair.token0)) {
+      items0.push(transactionItem.id);
+    } else if (vaultB.equals(pair.token1)) {
+      items1.push(transactionItem.id);
+    }
+  }
+
   const transaction = getOrCreateTransaction(event);
   transaction.type = "Withdrawal";
-  transaction.user = getOrCreateUser(event.params.to).id;
+  transaction.user = getOrCreateUser(params.to).id;
+  transaction.items0 = items0;
+  transaction.items1 = items1;
   transaction.save();
 }
