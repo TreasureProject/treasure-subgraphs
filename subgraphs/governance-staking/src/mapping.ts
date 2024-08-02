79639a096b80e68768118baad360f2dd3c6d9a04
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-08-02 09:52:30 -0700

    add governance staking subgraph

---
+++ subgraphs/governance-staking/src/mapping.ts
@@ -0,0 +1,40 @@
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+
+import {
+  Deposit,
+  Withdraw,
+} from "../generated/Governance Staking/GovernanceStaking";
+import { User, UserBalance } from "../generated/schema";
+
+const getOrCreateUser = (id: Address): User => {
+  let user = User.load(id);
+  if (!user) {
+    user = new User(id);
+  }
+
+  return user;
+};
+
+const getOrCreateBalance = (userId: Address): UserBalance => {
+  let balance = UserBalance.load(userId);
+  if (!balance) {
+    balance = new UserBalance(userId);
+    balance.balance = BigInt.zero();
+  }
+
+  return balance;
+};
+
+export function handleDeposit(event: Deposit): void {
+  const user = getOrCreateUser(event.params.user);
+  const balance = getOrCreateBalance(user.id);
+  balance.balance = balance.balance.plus(event.params.amount);
+  balance.save();
+}
+
+export function handleWithdraw(event: Withdraw): void {
+  const user = getOrCreateUser(event.params.user);
+  const balance = getOrCreateBalance(user.id);
+  balance.balance = balance.balance.minus(event.params.amount);
+  balance.save();
+}
