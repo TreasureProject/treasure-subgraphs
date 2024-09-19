2c0cc614845e5eaf100cc4fe87f8b16a896a689d
crisis <cdotta90@gmail.com>
2024-09-19 09:00:55 -0300

    feat: add buy/sell and graduation events

--- subgraphs/memecoins/src/mapping.ts
+++ subgraphs/memecoins/src/mapping.ts
@@ -1,14 +1,53 @@
-import { MemeMade } from "../generated/Meme Presale/MemePresale";
+import { Address } from "@graphprotocol/graph-ts";
+
+import {
+  BuySell,
+  Graduation,
+  MemeMade,
+} from "../generated/Meme Presale/MemePresale";
 import { MemePresale } from "../generated/schema";
+
+const getOrCreateMemePresale = (memecoin: Address): MemePresale => {
+  let memePresale = MemePresale.load(memecoin);
+  if (!memePresale) {
+    memePresale = new MemePresale(memecoin);
+  }
+
+  return memePresale;
+};
 
 export function handleMemeMade(event: MemeMade): void {
   const params = event.params;
-  const memePresale = new MemePresale(params.presaleinfo.memecoin);
+  const memePresale = getOrCreateMemePresale(params.presaleinfo.memecoin);
 
   memePresale.name = params.name;
   memePresale.symbol = params.symbol;
   memePresale.uri = params.uri;
   memePresale.amount = params.amount;
+  memePresale.targetMagicToRaise = params.presaleinfo.targetMagicToRaise;
+  memePresale.presalePrice = params.presaleinfo.presalePrice;
+  memePresale.magicRaised = params.presaleinfo.magicRaised;
+  memePresale.totalsupply = params.presaleinfo.totalsupply;
+  memePresale.graduated = params.presaleinfo.graduated;
+
+  memePresale.save();
+}
+
+export function handleBuySell(event: BuySell): void {
+  const params = event.params;
+  const memePresale = getOrCreateMemePresale(params.presaleinfo.memecoin);
+  memePresale.targetMagicToRaise = params.presaleinfo.targetMagicToRaise;
+  memePresale.presalePrice = params.presaleinfo.presalePrice;
+  memePresale.magicRaised = params.presaleinfo.magicRaised;
+  memePresale.totalsupply = params.presaleinfo.totalsupply;
+  memePresale.graduated = params.presaleinfo.graduated;
+
+  memePresale.save();
+}
+
+export function handleGraduation(event: Graduation): void {
+  const params = event.params;
+  const memePresale = getOrCreateMemePresale(params.presaleinfo.memecoin);
   memePresale.targetMagicToRaise = params.presaleinfo.targetMagicToRaise;
   memePresale.presalePrice = params.presaleinfo.presalePrice;
   memePresale.magicRaised = params.presaleinfo.magicRaised;
